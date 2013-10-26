9011d35716f369e7011b422075087aaf7349a65b
Adam Schneider <spotchious@gmail.com>
2013-10-25 21:05:19 -0500

    - serializing nulls

--- src/main/java/game/ai/Gson.java
+++ src/main/java/game/ai/Gson.java
@@ -2,9 +2,15 @@ package game.ai;
 
 public class Gson implements IGson {
 
+	com.google.gson.Gson _gson;
+	
+	public Gson() {
+		_gson = new com.google.gson.GsonBuilder().serializeNulls().create();
+	}
+	
 	@Override
 	public String toJson(Object deserializedObject) {
-		return new com.google.gson.Gson().toJson(deserializedObject);
+		return _gson.toJson(deserializedObject);
 	}
 
 }
